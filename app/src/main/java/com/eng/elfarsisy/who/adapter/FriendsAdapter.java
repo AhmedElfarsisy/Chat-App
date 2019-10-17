4a59179a3d9221061264dfb1daa5929616de4f53
ahmedelfarsisy <ahmedelfarsisy96@gmail.com>
2019-10-17 15:23:45 +0200

    friends recycler

--- app/src/main/java/com/eng/elfarsisy/who/adapter/FriendsAdapter.java
+++ app/src/main/java/com/eng/elfarsisy/who/adapter/FriendsAdapter.java
@@ -1,32 +1,65 @@
 package com.eng.elfarsisy.who.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FriendsHolder>{
+import com.bumptech.glide.Glide;
+import com.eng.elfarsisy.who.R;
+import com.eng.elfarsisy.who.model.User;
+
+import java.util.List;
+
+public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FriendsHolder> {
+    Context mContext;
+    List<User> mData;
+
+    public FriendsAdapter() {
+    }
+
+    public FriendsAdapter(Context mContext, List<User> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
 
     @NonNull
     @Override
     public FriendsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View inflate = LayoutInflater.from(mContext).inflate(R.layout.friend_item, parent, false);
+
+
+        return new FriendsHolder(inflate);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FriendsHolder holder, int position) {
-
+        holder.friendName.setText(mData.get(position).getName());
+        Glide.with(mContext).load(mData.get(position).getPersonalimage()).into(holder.friendImage);
     }
+
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData != null ? mData.size() : 0;
     }
 
     public class FriendsHolder extends RecyclerView.ViewHolder {
-            public FriendsHolder(@NonNull View itemView) {
-                super(itemView);
-            }
+        ImageView friendImage;
+        TextView friendName;
+
+        public FriendsHolder(@NonNull View itemView) {
+            super(itemView);
+            friendImage = itemView.findViewById(R.id.friendImage);
+            friendName = itemView.findViewById(R.id.frindename);
+
+
         }
+    }
 }
--- app/src/main/java/com/eng/elfarsisy/who/ui/home/HomeFragment.java
+++ app/src/main/java/com/eng/elfarsisy/who/ui/home/HomeFragment.java
@@ -11,19 +11,76 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.eng.elfarsisy.who.R;
+import com.eng.elfarsisy.who.adapter.FriendsAdapter;
+import com.eng.elfarsisy.who.model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    RecyclerView friendsRecycler;
+    FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    List<User> friendsList;
+    FriendsAdapter friendsAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        friendsRecycler = root.findViewById(R.id.recycle_Friends);
+        friendsRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Users");
+        friendsRecycler.setHasFixedSize(true);
+
 
         return root;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                friendsList = new ArrayList<>();
+                for (DataSnapshot friendData : dataSnapshot.getChildren()) {
+
+                    User friend = friendData.getValue(User.class);
+                    if (!friend.equals(firebaseAuth.getCurrentUser())) {
+
+                        friendsList.add(friend);
+                    }
+                }
+                friendsAdapter = new FriendsAdapter(getActivity(), friendsList);
+                friendsRecycler.setAdapter(friendsAdapter);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
